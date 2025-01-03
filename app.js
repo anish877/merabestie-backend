9ac7f90cf4a938a8eb9c6f408936349b3feb57ed
anish877 <62011810+anish877@users.noreply.github.com>
2025-01-03 20:22:31 +0530

    cors fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000','https://merabestie.com','https://merabestie-eosin.vercel.app'], 
+  origin: ['http://localhost:3000','https://merabestie.com','https://merabestie-eosin.vercel.app','https://merabestie-redesignes-7b99njdwk-anish877s-projects.vercel.app/'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
